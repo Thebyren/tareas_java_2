3b704bf6736054b7fce303ebc4387e41cdd3a3f1
byron pastor <99785073+Thebyren@users.noreply.github.com>
2024-07-22 18:24:48 -0600

    Update Main.java

--- exer/wrk2/Main.java
+++ exer/wrk2/Main.java
@@ -1,7 +1,24 @@
 package exer.wrk2;
-import exer.wrk2.grupo1;
-
+import exer.wrk2.grupo1.grupo1;
+import exer.wrk2.grupo2.grupo2;
+import exer.wrk2.grupo3.grupo3;
 public class Main{
-public static void Main(String[] args){
+public static void main(String[] args){
+
+
+ // grupo 1
  grupo1.volCube(2);
+ grupo1.cirCicle(2);
+ grupo1.volCube(2);
+ 
+ //grupo 2
+ grupo2.areaPiramid(2,3);
+ grupo2.volumPiramid(2,3);
+ grupo2.areaTrian(2,3);
+
+ //grupo 2
+ grupo3.volumPrism(2,3);
+ grupo3.areaParalegrm(2,3);
+ grupo3.volumCilindr(2,3);
+
 }}
