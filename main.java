8fc6375761f2b86a974549e5d6123c1aa280b22e
byron pastor <99785073+Thebyren@users.noreply.github.com>
2024-07-27 11:01:52 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+import Conductores.ConductorCoche;
+import Conductores.ConductorMoto;
+
+import Vehiculos.Moto;
+import Vehiculos.Coche;
+
+import Vehiculos.Vehiculo;
+import Conductores.Conductor;
+
+class main {
+    public static void main(String[] args) {
+        Conductor alex = new ConductorCoche();
+    }
+}
